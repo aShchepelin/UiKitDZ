da9040febe9f5d69ff2ebc2fcd9da13c0f243111
aShchepelin <aleksandr.shchepelin@ru.nestle.com>
2022-09-22 13:45:03 +0300

    сделал 2е приложение в 3ДЗ

--- UiKitDZ/UiKitDZ/ViewController.swift
+++ UiKitDZ/UiKitDZ/ViewController.swift
@@ -6,11 +6,33 @@
 //
 
 import UIKit
-
-final class ViewController: UIViewController {
-
+/// ViewController
+class ViewController: UIViewController {
+    let textLabel: UILabel = {
+       var label = UILabel()
+        label.frame = CGRect(x: 120, y: 200, width: 70, height: 70)
+        return label
+    }()
+    let startButton: UIButton = {
+       let button = UIButton()
+        button.frame = CGRect(x: 120, y: 100, width: 150, height: 70)
+        button.setTitle("Начать", for: .normal)
+        button.backgroundColor = .orange
+        return button
+    }()
+    var text = String()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
+        startButton.addTarget(self, action: #selector(startGame), for: .touchUpInside)
+    }
+    func setupUI() {
+        view.addSubview(startButton)
+        view.addSubview(textLabel)
+    }
+    
+    @objc func startGame() {
+        helloGame()
     }
 }
