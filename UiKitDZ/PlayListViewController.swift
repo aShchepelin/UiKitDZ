2de23e9146cbd62b87c4cf6cbf44f79a3a05db12
aShchepelin <aleksandr.shchepelin@ru.nestle.com>
2022-09-25 16:08:06 +0300

    сделал 6 задание

--- UiKitDZ/PlayListViewController.swift
+++ UiKitDZ/PlayListViewController.swift
@@ -6,11 +6,37 @@
 //
 
 import UIKit
-/// Плейлист
+/// Плейлист с кнопками которые передают информацию на экран плеера
 class PlayListViewController: UIViewController {
-
+    
+    var counter = Int()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      setupUI()
     }
+    
+    func setupUI() {
+    }
+    @IBAction func dunaAction(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let secondVC = storyboard.instantiateViewController(
+            withIdentifier: "PlayerViewController") as? PlayerViewController else { return }
+        secondVC.song = Song(name: "Привет с большого бодуна",
+                                           artist: "Дюна",
+                                           albumImage: "1",
+                                           song: "Duna - privet s bolshogo boduna")
+        show(secondVC, sender: nil)
+    }
+    
+    @IBAction func laertskiyAction(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let secondVC = storyboard.instantiateViewController(
+            withIdentifier: "PlayerViewController") as? PlayerViewController else { return }
+        secondVC.song = Song(name: "Дети хоронят коня",
+                                           artist: "Александр Лаэртский",
+                                           albumImage: "2",
+                                           song: "Aleksandr Laertskij  - Deti horonyat konya")
+        show(secondVC, sender: nil)
+    }
+    
 }
