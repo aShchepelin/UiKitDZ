048f6aa27f81e4f49f1ccf0d6e8f359436a57e87
aShchepelin <aleksandr.shchepelin@ru.nestle.com>
2022-10-01 19:31:19 +0300

    добавил сцен делегат

--- UiKitDZ/SceneDelegate.swift
+++ UiKitDZ/SceneDelegate.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 /// SceneDelegate
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-var window: UIWindow?
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let applicationScene = (scene as? UIWindowScene) else { return }
+        window?.windowScene = applicationScene
+        window?.makeKeyAndVisible()
+        
+        let startViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: startViewController)
+        window?.rootViewController = navigationController
+
+    }
 }
