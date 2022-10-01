a7734a6b231a7ac7b9509f8c521cd7722b2a53a3
aShchepelin <aleksandr.shchepelin@ru.nestle.com>
2022-10-01 19:27:26 +0300

    сделал 11 ДЗ

---
+++ UiKitDZ/TimerViewController.swift
@@ -0,0 +1,83 @@
+//
+//  TimerViewController.swift
+//  UiKitDZ
+//
+//  Created by Александр Андреевич Щепелин on 01.10.2022.
+//
+
+import UIKit
+/// Экран таймера с добавлением секунд
+final class TimerViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var timer: UIPickerView!
+    
+    // MARK: - Public properties
+    
+    var hours = 0
+    var minutes = 0
+    var seconds = 0
+    
+    // MARK: - UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        timer.delegate = self
+        timer.dataSource = self
+    }
+}
+
+/// Расширение пикервью для добавления значений
+
+extension TimerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 25
+        case 1, 2:
+            return 60
+            
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width/3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row) ч"
+        case 1:
+            return "\(row) мин"
+        case 2:
+            return "\(row) с"
+        default:
+            return ""
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            hours = row
+        case 1:
+            minutes = row
+        case 2:
+            seconds = row
+        default:
+            break
+        }
+    }
+}
